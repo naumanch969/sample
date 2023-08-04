41697b65b2269352d7ce07ebb24cde02c1fdca95
naumanch969 <naumanch969@gmail.com>
2023-08-04 08:19:55 +0500

    simple react app

--- server/routes/auth.js
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-import { register, login } from '../controllers/auth.js'
-
-const router = express.Router()
-
-router.post('/register', register)
-router.put('/login', login)
-
-export default router
--- server/utils/error.js
+++
@@ -1,7 +0,0 @@
-
-export const error = (status,message)=>{
-    const err = new Error()
-    err.status = status
-    err.message  = message
-    return err
-}
